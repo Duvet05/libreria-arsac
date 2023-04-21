cffafc6caad8f42ae5e87399b8c795491172f982
Mauricio Arenales <arenales.mauriciol@pucp.edu.pe>
2023-04-21 17:27:21 -0500

    impresion de prueba 2

--- ARSACSoft/src/arsacsoft/ARSACSoft.java
+++ ARSACSoft/src/arsacsoft/ARSACSoft.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arsacsoft;
-
-/**
- *
- * @author duvet
- */
 public class ARSACSoft {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Mauricio");
     }
     
 }
