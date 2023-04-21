7a18ef5f128ce258eb5071db2eee841efebf0313
Mauricio Arenales <arenales.mauriciol@pucp.edu.pe>
2023-04-21 11:50:39 -0500

    impresion de prueba

--- ARSACSoftProductos/src/arsacsoftproductos/ARSACSoftProductos.java
+++ ARSACSoftProductos/src/arsacsoftproductos/ARSACSoftProductos.java
@@ -14,7 +14,7 @@ public class ARSACSoftProductos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
