d9e94f72e43253914a28c8ee6ec2c2328c392422
Gino Llano <112000359+gino-llano@users.noreply.github.com>
2023-06-01 01:38:33 -0500

    Se agregan nuevos scripts

--- Scripts/sp_arsac.sql
+++ Scripts/sp_arsac.sql
@@ -1,30 +1,92 @@
+DROP PROCEDURE IF EXISTS INSERTAR_MARCA;
+DROP procedure IF exists INSERTAR_CATEGORIA;
+
+DROP procedure IF exists INSERTAR_PRODUCTO;
+DROP procedure IF exists LISTAR_PRODUCTOS;
+
+DROP procedure IF exists INSERTAR_TIPO_EMPLEADO;
+DROP procedure IF exists INSERTAR_SEDE;
 
 DROP PROCEDURE IF EXISTS INSERTAR_EMPLEADO;
-DROP PROCEDURE IF EXISTS ACTUALIZAR_EMPLEADO;
-DROP PROCEDURE IF EXISTS ELIMINAR_EMPLEADO;
-DROP PROCEDURE IF EXISTS LISTAR_EMPLEADOS;
-DROP PROCEDURE IF EXISTS OBTENER_EMPLEADO;
-DROP PROCEDURE IF EXISTS BUSCAR_EMPLEADO;
-DROP PROCEDURE IF EXISTS VERIFICAR_EMPLEADO;
 
-DROP PROCEDURE IF EXISTS INSERTAR_VENDEDOR;
+DROP PROCEDURE IF EXISTS INSERTAR_CLIENTE_MAYORISTA;
+DROP PROCEDURE IF EXISTS LISTAR_CLIENTES_MAYORISTAS;
 
-DROP PROCEDURE IF EXISTS LISTAR_PRODUCTOS;
-DROP PROCEDURE IF EXISTS ACTUALIZAR_PRODUCTO;
-DROP PROCEDURE IF EXISTS OBTENER_PRODUCTO;
-DROP PROCEDURE IF EXISTS INSERTAR_PRODUCTO;
-DROP PROCEDURE IF EXISTS MODIFICAR_PRODUCTO;
-DROP PROCEDURE IF EXISTS ELIMINAR_PRODUCTO;
-DROP PROCEDURE IF EXISTS REGISTRAR_VENTA;
+DROP PROCEDURE IF EXISTS INSERTAR_SEDE_X_PRODUCTO;
+DROP PROCEDURE IF EXISTS LISTAR_SEDE_X_PRODUCTO;
 
-DROP PROCEDURE IF EXISTS INSERTAR_SEDE;
-DROP PROCEDURE IF EXISTS BUSCAR_SEDE_POR_DIRECCION;
+DROP PROCEDURE IF EXISTS INSERTAR_ORDEN_DE_VENTA;
+DROP PROCEDURE IF EXISTS INSERTAR_LINEA_ORDEN_DE_VENTA;
+DROP PROCEDURE IF EXISTS LISTAR_LINEA_ORDEN_DE_VENTA;
 
-DROP PROCEDURE IF EXISTS INSERTAR_FACTURA;
-DROP PROCEDURE IF EXISTS INSERTAR_BOLETA;
+DROP PROCEDURE IF EXISTS INSERTAR_ORDEN_DE_VENTA_MAYORISTA;
 
 DELIMITER $
+
+-- Producto
+CREATE PROCEDURE INSERTAR_MARCA(
+	OUT _id_marca INT,
+    IN _descripcion VARCHAR(50)
+)
+BEGIN
+	INSERT INTO marca(descripcion, activo)
+    values(_descripcion, true);
+	SET _id_marca = LAST_INSERT_ID();
+END $
+
+CREATE PROCEDURE INSERTAR_CATEGORIA(
+	OUT _id_categoria INT,
+    IN _descripcion VARCHAR(50)
+)
+BEGIN
+	INSERT INTO categoria(descripcion, activo) values(_descripcion, true);
+	SET _id_categoria = LAST_INSERT_ID();
+END $
+
+CREATE PROCEDURE INSERTAR_PRODUCTO(
+	OUT _idProducto int,
+    IN _idCategoria INT,
+    IN _idMarca INT,
+    IN _nombre VARCHAR(50),
+    IN _precio DECIMAL(10, 2),
+    IN _precioPorMayor DECIMAL(10, 2)
+)
+BEGIN
+    INSERT INTO producto(idCategoria, idMarca, nombre, precio, precioPorMayor, activo)
+    VALUES(_idCategoria, _idMarca, _nombre, _precio, _precioPorMayor, true);
+    SET _idProducto = LAST_INSERT_ID();
+END $
+
+CREATE PROCEDURE LISTAR_PRODUCTOS()
+BEGIN
+    SELECT *
+    FROM producto
+    WHERE activo = true;
+END $
+
 -- Empleado
+CREATE PROCEDURE INSERTAR_TIPO_EMPLEADO(
+	out _idTipoEmpleado INT,
+    IN _descripcion VARCHAR(50)
+)
+BEGIN
+	INSERT INTO tipoEmpleado(descripcion, activo)
+    VALUES(_descripcion, true);
+	SET _idTipoEmpleado = LAST_INSERT_ID();
+END $
+
+CREATE PROCEDURE INSERTAR_SEDE(
+	OUT _idSede INT,
+    IN _esAlmacen BOOLEAN,
+    IN _direccion VARCHAR(50),
+    IN _telefono VARCHAR(50),
+    IN _correo VARCHAR(50)
+)
+BEGIN
+	INSERT INTO sede(esAlmacen, direccion, telefono, correo, activo)
+    VALUES(_esAlmacen, _direccion, _telefono, _correo, true);
+	SET _idSede = last_insert_id();
+END $
 
 CREATE PROCEDURE INSERTAR_EMPLEADO(
     OUT _id_empleado INT,
@@ -33,251 +95,120 @@ CREATE PROCEDURE INSERTAR_EMPLEADO(
     IN _apellidos VARCHAR(70),
     IN _correo VARCHAR(70),
     IN _telefono VARCHAR(70),
-    IN _cargo VARCHAR(70),
+    IN _idTipoEmpleado INT,
     IN _fechaContratacion DATE,
     IN _salario DECIMAL(10,2),
     IN _direccion VARCHAR(100),
-    IN _contrasena VARCHAR(50)
+    IN _usuario VARCHAR(50),
+    IN _contrasena VARCHAR(50),
+    IN _idSede INT
 )
 BEGIN
-    INSERT INTO persona(nombre, apellidos, DNI, correo, telefono, activo) VALUES(_nombre,_apellidos,_DNI,_correo, _telefono, 1);
+    INSERT INTO persona(nombre, apellidos, DNI, correo, telefono, activo)
+    VALUES(_nombre,_apellidos,_DNI,_correo, _telefono, true);
     SET _id_empleado = LAST_INSERT_ID();
-    INSERT INTO empleado(idEmpleado,cargo,fechaContratacion,salario,direccion, contrasena) 
-            VALUES(_id_empleado,_cargo,_fechaContratacion,_salario,_direccion, _contrasena);
+    INSERT INTO empleado(idEmpleado,idTipoEmpleado,fechaContratacion,salario,direccion, contrasena,idSede) 
+            VALUES(_id_empleado,_idTipoEmpleado,_fechaContratacion,_salario,_direccion, _contrasena,_idSede);
 END $
 
-CREATE PROCEDURE ELIMINAR_EMPLEADO(IN _idEmpleado INT)
-BEGIN
-    DECLARE empCount INT DEFAULT 0;
-    SELECT COUNT(*) INTO empCount FROM empleado WHERE idEmpleado = _idEmpleado;
-    IF empCount = 0 THEN
-        SELECT 'Error: Empleado no encontrado.' AS 'Mensaje';
-    ELSE
-        UPDATE persona p
-        INNER JOIN empleado e ON p.idUsuario = e.idEmpleado
-        SET p.activo = false
-        WHERE e.idEmpleado = _idEmpleado;
-    END IF;
-END $
-
-CREATE PROCEDURE LISTAR_EMPLEADOS()
-BEGIN
-    SELECT idUsuario, nombre, apellidos, DNI, correo, telefono, cargo, fechaContratacion, salario, direccion
-    FROM persona p 
-    INNER JOIN empleado e ON p.idUsuario = e.idEmpleado
-    WHERE p.activo = true
-    UNION
-    SELECT idUsuario, nombre, apellidos, DNI, correo, telefono, cargo, fechaContratacion, salario, direccion 
-    FROM persona p 
-    INNER JOIN empleado e ON p.idUsuario = e.idEmpleado 
-    INNER JOIN vendedor v ON v.idVendedor = e.idEmpleado
-    WHERE p.activo = true;
-END $
-
-CREATE PROCEDURE OBTENER_EMPLEADO(IN _idEmpleado INT)
-BEGIN
-    SELECT p.idUsuario, p.nombre, p.apellidos, p.DNI, p.correo, p.telefono, e.cargo, e.fechaContratacion, e.salario, e.direccion
-    FROM persona p
-    INNER JOIN empleado e ON p.idUsuario = e.idEmpleado
-    WHERE p.activo = true AND p.idUsuario = _idEmpleado;
-END $
-
-CREATE PROCEDURE BUSCAR_EMPLEADO(IN nombre VARCHAR(50))
-BEGIN
-    SELECT e.idEmpleado, p.nombre, p.apellidos, p.DNI, p.correo, e.contrasena, p.telefono, e.cargo, e.fechaContratacion, e.salario, e.direccion
-    FROM persona p
-    INNER JOIN empleado e ON p.idUsuario = e.idEmpleado
-    WHERE p.nombre LIKE CONCAT('%', nombre, '%') AND p.activo = 1;
-END $
-
-
-CREATE PROCEDURE VERIFICAR_EMPLEADO(
-    IN email VARCHAR(50),
-    IN _password VARCHAR(50),
-    OUT empleadoEncontrado BOOLEAN
-)
-BEGIN
-    DECLARE empleadoID INT;
-    DECLARE empleadoPassword VARCHAR(50);
-    DECLARE empleadoActivo BOOLEAN;
-    
-    SELECT idEmpleado, contrasena, activo
-    INTO empleadoID, empleadoPassword, empleadoActivo
-    FROM empleado e
-    JOIN persona p ON e.idEmpleado = p.idUsuario
-    WHERE p.correo = email;
-    
-    IF empleadoID IS NOT NULL AND empleadoPassword = _password AND empleadoActivo THEN
-        SET empleadoEncontrado = TRUE;
-    ELSE
-        SET empleadoEncontrado = FALSE;
-    END IF;
-END $
-
-
--- Vendedor
-CREATE PROCEDURE INSERTAR_VENDEDOR(
-	OUT _id_empleado INT,
+CREATE PROCEDURE INSERTAR_CLIENTE_MAYORISTA(
+	OUT _idClienteMayorista INT,
+    IN _DNI VARCHAR(8),
     IN _nombre VARCHAR(70),
     IN _apellidos VARCHAR(70),
-    IN _DNI VARCHAR(8),
     IN _correo VARCHAR(70),
     IN _telefono VARCHAR(70),
-    IN _cargo varchar(70),
-    IN _fechaContratacion DATE,
-    IN _salario decimal(10,2),
-	IN _direccion VARCHAR(100),
-    IN _contrasena VARCHAR(50),
-    IN _cantidadVentas INT,
-    IN _totalVendido decimal(10,2)
+    IN _RUC VARCHAR(50),
+    IN _razonSocial VARCHAR(50)
 )
 BEGIN
-	INSERT INTO persona(nombre, apellidos, DNI, correo, telefono, activo) VALUES(_nombre,_apellidos,_DNI,_correo, _telefono, 1);
-    SET _id_empleado = @@last_insert_id;
-    INSERT INTO empleado(idEmpleado,cargo,fechaContratacion,salario,direccion, contrasena) 
-			VALUES(_id_empleado,_cargo,_fechaContratacion,_salario,_direccion, _contrasena);
-	INSERT INTO vendedor(idVendedor, cantVentas, totalVendido) values(_id_empleado, _cantidadVentas, _totalVendido);
+    INSERT INTO persona(nombre, apellidos, DNI, correo, telefono, activo)
+    VALUES(_nombre,_apellidos,_DNI,_correo, _telefono, true);
+	SET _idClienteMayorista = last_insert_id();
+    INSERT INTO clienteMayorista(RUC, razonSocial)
+    VALUES(_RUC, _razonSocial);
 END $
 
-
--- Producto
-CREATE PROCEDURE INSERTAR_PRODUCTO(
-    IN _marca VARCHAR(50),
-    IN _nombreProducto VARCHAR(50),
-    IN _descripcion VARCHAR(100),
-    IN _tipoProducto VARCHAR(50),
-    IN _precioUnit DECIMAL(10, 2),
-    IN _precioPorMayor DECIMAL(10, 2),
-    IN _stockBase INT,
-    IN _stock INT
-)
+CREATE PROCEDURE LISTAR_CLIENTES_MAYORISTAS()
 BEGIN
-    INSERT INTO producto(marca, nombreProducto, descripcion, tipoProducto, precioUnit, precioPorMayor, stockBase, stock, agotado)
-    VALUES(_marca, _nombreProducto, _descripcion, _tipoProducto, _precioUnit, _precioPorMayor, _stockBase, _stock, false);
+	SELECT * FROM clienteMayorista;
 END $
 
-CREATE PROCEDURE ACTUALIZAR_PRODUCTO(
+CREATE PROCEDURE INSERTAR_SEDE_X_PRODUCTO(
+	IN _idSede INT,
     IN _idProducto INT,
-    IN _marca VARCHAR(50),
-    IN _nombreProducto VARCHAR(50),
-    IN _descripcion VARCHAR(100),
-    IN _tipoProducto VARCHAR(50),
-    IN _precioUnit DECIMAL(10, 2),
-    IN _precioPorMayor DECIMAL(10, 2),
-    IN _stockBase INT,
     IN _stock INT,
-    IN _agotado BOOLEAN
+    IN _stockBase INT,
+    IN _stockMaximo INT
 )
 BEGIN
-    UPDATE producto
-    SET marca = _marca,
-        nombreProducto = _nombreProducto,
-        descripcion = _descripcion,
-        tipoProducto = _tipoProducto,
-        precioUnit = _precioUnit,
-        precioPorMayor = _precioPorMayor,
-        stockBase = _stockBase,
-        stock = _stock,
-        agotado = _agotado
-    WHERE idProducto = _idProducto;
+	INSERT INTO sedeXproducto(idSede, idProducto, stock, stockBase, stockMaximo)
+    VALUES(_idSede, _idProducto, _stock, _stockBase, _stockMaximo);
 END $
 
-CREATE PROCEDURE ELIMINAR_PRODUCTO(IN _idProducto INT)
+CREATE PROCEDURE LISTAR_SEDE_X_PRODUCTO()
 BEGIN
-    UPDATE producto
-    SET activo = false
-    WHERE idProducto = _idProducto;
+	SELECT * FROM sedeXproducto;
 END $
 
-CREATE PROCEDURE LISTAR_PRODUCTOS()
+CREATE PROCEDURE INSERTAR_ORDEN_DE_VENTA(
+	OUT _idOrdenDeVenta INT,
+    IN _idEmpleado INT,
+    IN _precioTotal DECIMAL(10, 2)
+)
 BEGIN
-    SELECT idProducto, marca, nombreProducto, descripcion, tipoProducto, precioUnit, precioPorMayor, stockBase, stock, agotado
-    FROM producto
-    WHERE activo = true;
+	INSERT INTO ordenDeVenta(idEmpleado, fechaOrden, precioTotal, esMayorista, activo)
+    VALUES(_idEmpleado, current_date(), _precioTotal, false, true);
+	SET _idOrdenDeVenta = last_insert_id();
 END $
 
-CREATE PROCEDURE OBTENER_PRODUCTO(IN _idProducto INT)
-BEGIN
-    SELECT idProducto, marca, nombreProducto, descripcion, tipoProducto, precioUnit, precioPorMayor, stockBase, stock, agotado
-    FROM producto
-    WHERE activo = true AND idProducto = _idProducto;
-END $
-
-CREATE PROCEDURE REGISTRAR_VENTA(
+CREATE PROCEDURE INSERTAR_LINEA_ORDEN_DE_VENTA(
+	IN _idOrdenDeVenta INT,
     IN _idProducto INT,
     IN _cantidad INT,
-    OUT _ventaRegistrada BOOLEAN
+    IN _descuento DECIMAL(10,2),
+    IN _precio DECIMAL(10, 2)
 )
 BEGIN
-    DECLARE _stockActual INT;
+	DECLARE _esMayorista BOOLEAN;
+    DECLARE _idSede INT;
     
-    SELECT stock INTO _stockActual FROM producto WHERE idProducto = _idProducto AND activo = true;
+    INSERT INTO lineaOrdenDeVenta(idOrdenDeVenta,idProducto,cantidad,descuento,precio)
+    VALUES(_idOrdenDeVenta,_idProducto,_cantidad,_descuento,_precio);
     
-    IF _stockActual >= _cantidad THEN
-        UPDATE producto SET stock = _stockActual - _cantidad WHERE idProducto = _idProducto;
-        SET _ventaRegistrada = true;
-    ELSE
-        SET _ventaRegistrada = false;
+	SELECT esMayorista INTO _esMayorista FROM ordenDeVenta o
+	WHERE o.idOrdenDeVenta = _idOrdenDeVenta;
+    
+    IF _esMayorista = false THEN
+    
+		SELECT idSede INTO _idSede FROM empleado e INNER JOIN ordenDeVenta o
+		WHERE o.idEmpleado =  e.idEmpleado and o.idOrdenDeVenta = _idOrdenDeVenta;
+    
+		UPDATE sedeXproducto SET stock = stock - _cantidad
+        WHERE idProducto = _idProducto and idSede = _idSede;
+        
     END IF;
+    
 END $
 
--- SEDE
-CREATE PROCEDURE INSERTAR_SEDE(
-    IN _direccion VARCHAR(50),
-    IN _telefono VARCHAR(20),
-    IN _correo VARCHAR(50),
-    IN _horaIniAtencion TIME,
-    IN _horaFinAtencion TIME,
-    IN _descripcion VARCHAR(100),
-    IN _esPrincipal BOOLEAN
+CREATE PROCEDURE LISTAR_LINEA_ORDEN_DE_VENTA(
+	IN _idOrdenDeVenta int
 )
 BEGIN
-    INSERT INTO sede(direccion, telefono, correo, horaIniAtencion, horaFinAtencion, descripcion, esPrincipal) 
-    VALUES(_direccion, _telefono, _correo, _horaIniAtencion, _horaFinAtencion, _descripcion, _esPrincipal);
+	SELECT * FROM lineaOrdenDeVenta WHERE idOrdenDeVenta = _idOrdenDeVenta;
 END $
 
-CREATE PROCEDURE BUSCAR_SEDE_POR_DIRECCION(IN nombre_sede VARCHAR(50))
-BEGIN
-    SELECT idSede, direccion, telefono, correo, horaIniAtencion, horaFinAtencion, descripcion, esPrincipal
-    FROM sede
-    WHERE activo = true AND direccion LIKE CONCAT('%', nombre_sede, '%');
-END $
-
--- FACTURA Y BOLETA
-
-CREATE PROCEDURE INSERTAR_FACTURA(
-	IN _idPedido INT,
-	IN _idCliente INT,
-	IN _direccionFacturacion VARCHAR(200),
-	IN _tipoMoneda VARCHAR(3),
-	IN _totalVenta DECIMAL(10,2),
-	IN _igv DECIMAL(10,2),
-	IN _numSerie VARCHAR(4),
-	IN _numCorrelativo VARCHAR(8)
+CREATE PROCEDURE INSERTAR_ORDEN_DE_VENTA_MAYORISTA(
+	OUT _idOrdenDeVenta INT,
+    IN _idEmpleado INT,
+    IN _precioTotal DECIMAL(10, 2),
+    IN _idClienteMayorista INT
 )
 BEGIN
-	DECLARE _totalFactura DECIMAL(10,2);
-	SET _totalFactura = _totalVenta + _igv;
-	
-	INSERT INTO factura(idFactura, idCliente, direccioFacturacion, tipoMoneda, totalVenta, igv, totalFactura, numSerie, numCorrelativo)
-	VALUES (_idPedido, _idCliente, _direccionFacturacion, _tipoMoneda, _totalVenta, _igv, _totalFactura, _numSerie, _numCorrelativo);
-END $
-
-CREATE PROCEDURE INSERTAR_BOLETA(
-	IN _idPedido INT,
-	IN _nombreCliente VARCHAR(100),
-	IN _tipoDocumentoCliente VARCHAR(3),
-	IN _numDocumentoCliente VARCHAR(20),
-	IN _tipoMoneda VARCHAR(3),
-	IN _totalVenta DECIMAL(10,2),
-	IN _igv DECIMAL(10,2),
-	IN _descripcion VARCHAR(100)
-)
-BEGIN
-	DECLARE _totalBoleta DECIMAL(10,2);
-	SET _totalBoleta = _totalVenta + _igv;
-	
-	INSERT INTO boleta(idBoleta, nombreCliente, tipoDocumentoCliente, numDocumentoCliente, tipoMoneda, totalVenta, igv, totalBoleta, descripcion)
-	VALUES (_idPedido, _nombreCliente, _tipoDocumentoCliente, _numDocumentoCliente, _tipoMoneda, _totalVenta, _igv, _totalBoleta, _descripcion);
+	INSERT INTO ordenDeVenta(idEmpleado, fechaOrden, precioTotal, esMayorista, activo)
+    VALUES(_idEmpleado, current_date(), _precioTotal, true, true);
+	SET _idOrdenDeVenta = last_insert_id();
+    INSERT INTO ordenDeVentaMayorista(idOrdenDeVenta, idClienteMayorista, fechaEntrega)
+    VALUES(_idOrdenDeVenta, _idClienteMayorista, null);
 END $
 
---
+++ Scripts/tablas_arsac.sql
@@ -0,0 +1,239 @@
+DROP TABLE IF EXISTS ordenDeCompraXproveedorXproducto;
+DROP TABLE IF EXISTS ordenDeCompraXproveedor;
+DROP TABLE IF EXISTS ordenDeCompra;
+DROP TABLE IF EXISTS ordenDeVentaMayorista;
+DROP TABLE IF EXISTS lineaOrdenDeVenta;
+DROP TABLE IF EXISTS ordenDeVenta;
+DROP TABLE IF EXISTS lineaOrdenDeAbastecimiento;
+DROP TABLE IF EXISTS ordenDeAbastecimiento;
+DROP TABLE IF EXISTS productoXproveedor;
+DROP TABLE IF EXISTS sedeXproducto;
+DROP TABLE IF EXISTS promocion;
+DROP TABLE IF EXISTS producto;
+DROP TABLE IF EXISTS clienteMayorista;
+DROP TABLE IF EXISTS empleado;
+DROP TABLE IF EXISTS tipoEmpleado;
+DROP TABLE IF EXISTS proveedor;
+DROP TABLE IF EXISTS categoria;
+DROP TABLE IF EXISTS marca;
+DROP TABLE IF EXISTS sede;
+DROP TABLE IF EXISTS persona;
+DROP TABLE IF EXISTS parametros;
+
+CREATE TABLE parametros (
+  nombre_empresa VARCHAR(100) NOT NULL,
+  ruc VARCHAR(20) NOT NULL,
+  direccion VARCHAR(200) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  CONSTRAINT parametros_pk PRIMARY KEY (nombre_empresa)
+);
+
+CREATE TABLE persona (
+	idPersona INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    apellidos VARCHAR(100),
+    DNI CHAR(8),
+    correo VARCHAR(50),
+    telefono VARCHAR(20),
+    activo boolean not null default 1,
+    PRIMARY KEY(idPersona)
+)ENGINE=InnoDB;
+
+CREATE TABLE sede (
+	idSede INT AUTO_INCREMENT,
+    direccion VARCHAR(50),
+    telefono VARCHAR(20),
+    correo VARCHAR(50),
+    esAlmacen BOOLEAN not null default 0,
+    activo boolean not null default 1,
+    PRIMARY KEY (idSede)
+)ENGINE=InnoDB;
+
+CREATE TABLE marca(
+	idMarca INT AUTO_INCREMENT,
+	descripcion VARCHAR(50),
+	activo BOOLEAN not null default 1,
+	PRIMARY KEY (idMarca)
+)ENGINE=InnoDB;
+
+CREATE TABLE categoria(
+	idCategoria INT AUTO_INCREMENT,
+	descripcion VARCHAR(50),
+	activo BOOLEAN not null default 1,
+	PRIMARY KEY (idCategoria)
+)ENGINE=InnoDB;
+
+CREATE TABLE proveedor (
+	idProveedor INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    RUC VARCHAR(50),
+    direccion VARCHAR(100),
+    telefono VARCHAR(20),
+    activo boolean not null default 1,
+    PRIMARY KEY (idProveedor)
+)ENGINE=InnoDB;
+
+CREATE TABLE tipoEmpleado(
+	idTipoEmpleado INT auto_increment,
+    descripcion VARCHAR(50),
+    activo BOOLEAN NOT NULL DEFAULT 1,
+    PRIMARY KEY (idTipoEmpleado)
+)ENGINE=InnoDB;
+
+CREATE TABLE empleado (
+	idEmpleado INT,
+	idSede INT,
+    fechaContratacion DATE,
+    salario DECIMAL(10, 2),
+    direccion VARCHAR(100),
+	usuario VARCHAR(50),
+    contrasena VARCHAR(50),
+    idTipoEmpleado INT,
+    PRIMARY KEY (idEmpleado),
+    FOREIGN KEY (idEmpleado) REFERENCES persona(idPersona),
+	FOREIGN KEY (idSede) REFERENCES sede(idSede),
+    FOREIGN KEY (idTipoEmpleado) REFERENCES tipoEmpleado(idTipoEmpleado)
+)ENGINE=InnoDB;
+
+CREATE TABLE clienteMayorista (
+	idClienteMayorista INT,
+    RUC VARCHAR(50),
+    razonSocial VARCHAR(50),
+    PRIMARY KEY (idClienteMayorista),
+    FOREIGN KEY (idClienteMayorista) REFERENCES persona(idPersona)
+)ENGINE=InnoDB;
+
+CREATE TABLE producto (
+	idProducto INT AUTO_INCREMENT,
+	idCategoria INT,
+	idMarca INT,
+	nombre VARCHAR(50),
+    precio DECIMAL(10, 2),
+    precioPorMayor DECIMAL(10, 2),
+    activo boolean not null default 1,
+    PRIMARY KEY (idProducto),
+    FOREIGN KEY (idCategoria) references categoria(idCategoria),
+    FOREIGN KEY (idMarca) references marca(idMarca)
+)ENGINE=InnoDB;
+
+CREATE TABLE promocion (
+	idProducto INT,
+    porcentaje DECIMAL(10, 2),
+    cantidadMinima INT,
+    fechaInicio DATE,
+	fechaFin DATE,
+    activo boolean not null default 1,
+    PRIMARY KEY (idProducto),
+	FOREIGN KEY (idProducto) REFERENCES producto(idProducto) 
+)ENGINE=InnoDB;
+
+CREATE TABLE sedeXproducto (
+	idSede INT,
+	idProducto INT,
+	stock INT,
+	stockBase INT,
+	stockMaximo INT,
+	activo boolean not null default 1,
+	PRIMARY KEY (idSede, idProducto),
+	FOREIGN KEY (idSede) REFERENCES sede(idSede),
+	FOREIGN KEY (idProducto) REFERENCES producto(idProducto) 
+)ENGINE=InnoDB;
+
+CREATE TABLE productoXproveedor(
+	idProducto INT,
+	idProveedor INT,
+	costo DECIMAL(10, 2),
+	activo boolean not null default 1,
+	PRIMARY KEY (idProducto, idProveedor),
+	FOREIGN KEY (idProveedor) REFERENCES proveedor(idProveedor),
+	FOREIGN KEY (idProducto) REFERENCES producto(idProducto) 
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeAbastecimiento(
+	idOrdenDeAbastecimiento INT auto_increment,
+	idEmpleado INT,
+	idSede INT,
+	fechaOrden DATE,
+	fechaEntrega DATE,
+    estado VARCHAR(50),
+    activo BOOLEAN NOT NULL DEFAULT 1,
+	PRIMARY KEY (idOrdenDeAbastecimiento),
+	FOREIGN KEY (idEmpleado) REFERENCES empleado(idEmpleado),
+	FOREIGN KEY (idSede) REFERENCES sede(idSede)
+)ENGINE=InnoDB;
+
+CREATE TABLE lineaOrdenDeAbastecimiento (
+	idOrdenDeAbastecimiento INT,
+	idProducto INT,
+	cantidad INT,
+	PRIMARY KEY (idOrdenDeAbastecimiento, idProducto),
+	FOREIGN KEY (idOrdenDeAbastecimiento) REFERENCES ordenDeAbastecimiento(idOrdenDeAbastecimiento),
+	FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeVenta (
+	idOrdenDeVenta INT AUTO_INCREMENT,
+    idEmpleado INT,
+	precioTotal DECIMAL(10, 2),
+    fechaOrden DATE,
+    esMayorista BOOLEAN,
+    activo BOOLEAN NOT NULL DEFAULT 1,
+    PRIMARY KEY (idOrdenDeVenta),
+    FOREIGN KEY (idEmpleado) REFERENCES empleado(idEmpleado)
+)ENGINE=InnoDB;
+
+CREATE TABLE lineaOrdenDeVenta (
+	idOrdenDeVenta INT,
+    idProducto INT,
+    cantidad INT,
+    descuento DECIMAL(10, 2),
+    precio DECIMAL(10,2),
+    activo BOOLEAN NOT NULL DEFAULT 1,
+    PRIMARY KEY (idOrdenDeVenta, idProducto),
+    FOREIGN KEY (idOrdenDeVenta) REFERENCES ordenDeVenta(idOrdenDeVenta),
+    FOREIGN KEY (idProducto) REFERENCES producto(idProducto) 
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeVentaMayorista(
+	idOrdenDeVenta INT,
+    idClienteMayorista INT,
+    fechaEntrega DATE,
+    PRIMARY KEY (idOrdenDeVenta),
+    FOREIGN KEY (idOrdenDeVenta) REFERENCES ordenDeVenta(idOrdenDeVenta),
+    FOREIGN KEY (idClienteMayorista) REFERENCES clienteMayorista(idClienteMayorista)
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeCompra (
+	idOrdenDeCompra INT AUTO_INCREMENT,
+	idEmpleado INT,
+	fechadeOrden DATE,
+	costoTotal DECIMAL(10, 2),
+    activo BOOLEAN NOT NULL DEFAULT 1,
+	PRIMARY KEY (idOrdenDeCompra),
+	FOREIGN KEY (idEmpleado) REFERENCES empleado(idEmpleado)
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeCompraXproveedor (
+    idOrdenDeCompra INT,
+	idProveedor INT,
+    fechaLlegada DATE,
+    costo DECIMAL(10, 2),
+    primary key (idOrdenDeCompra, idProveedor),
+    FOREIGN KEY (idOrdenDeCompra) REFERENCES ordenDeCompra(idOrdenDeCompra),
+    FOREIGN KEY (idProveedor) REFERENCES proveedor(idProveedor)
+)ENGINE=InnoDB;
+
+CREATE TABLE ordenDeCompraXproveedorXproducto (
+	idOrdenDeCompra INT,
+	idProveedor INT,
+    idProducto INT,
+    cantidad INT,
+    costo DECIMAL(10, 2),
+    PRIMARY KEY (idOrdenDeCompra, idProveedor, idProducto),
+    FOREIGN KEY (idOrdenDeCompra) REFERENCES ordenDeCompraXproveedor(idOrdenDeCompra),
+    FOREIGN KEY (idProveedor) REFERENCES ordenDeCompraXproveedor(idProveedor),
+    FOREIGN KEY (idProducto) REFERENCES producto(idProducto) 
+)Engine=InnoDB;
+
+
