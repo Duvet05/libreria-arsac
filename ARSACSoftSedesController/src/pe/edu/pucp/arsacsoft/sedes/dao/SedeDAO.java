4d35a5384e5a4f582943b37c02ac9006e81032fa
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-07 23:53:22 -0500

    CORRECCION BACK

--- ARSACSoftSedesController/src/pe/edu/pucp/arsacsoft/sedes/dao/SedeDAO.java
+++ ARSACSoftSedesController/src/pe/edu/pucp/arsacsoft/sedes/dao/SedeDAO.java
@@ -4,10 +4,16 @@
  */
 package pe.edu.pucp.arsacsoft.sedes.dao;
 
+import java.util.ArrayList;
+import pe.edu.pucp.arsacsoft.sedes.model.Sede;
+
 /**
  *
  * @author User
  */
 public interface SedeDAO {
-    
+    ArrayList<Sede> listarTodas();
+    int insertar(Sede sede);
+    int modificar(Sede sede);
+    int eliminar(int idsede);      
 }
