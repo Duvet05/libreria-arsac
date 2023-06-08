3a7a986cbfc712d5322328ca3553708c7201da92
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-07 23:18:57 -0500

    commit
    1

--- ARSACSoftAlmacenController/src/pe/edu/pucp/arsacsoft/almacen/dao/ProductoDAO.java
+++ ARSACSoftAlmacenController/src/pe/edu/pucp/arsacsoft/almacen/dao/ProductoDAO.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package pe.edu.pucp.arsacsoft.almacen.dao;
-
+import java.util.ArrayList;
+import pe.edu.pucp.arsacsoft.almacen.model.Producto;
 /**
  *
  * @author User
  */
 public interface ProductoDAO {
-      
+    ArrayList<Producto> listarTodas();
+    int insertar(Producto producto);
+    int modificar(Producto producto);
+    int eliminar(int idproducto);      
 }
