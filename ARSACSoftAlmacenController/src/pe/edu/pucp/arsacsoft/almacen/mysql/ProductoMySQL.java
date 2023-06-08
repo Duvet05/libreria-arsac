3f3853414b03488e9122eaf0066a535478f6b3ed
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-07 23:21:44 -0500

    Update ProductoMySQL.java

--- ARSACSoftAlmacenController/src/pe/edu/pucp/arsacsoft/almacen/mysql/ProductoMySQL.java
+++ ARSACSoftAlmacenController/src/pe/edu/pucp/arsacsoft/almacen/mysql/ProductoMySQL.java
@@ -4,10 +4,95 @@
  */
 package pe.edu.pucp.arsacsoft.almacen.mysql;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import pe.edu.pucp.arsacsoft.almacen.dao.ProductoDAO;
+import pe.edu.pucp.arsacsoft.almacen.model.Producto;
+import pe.edu.pucp.arsacsoft.config.DBManager;
+
 /**
  *
- * @author User
+ * @author User 
  */
-public class ProductoMySQL {
+
+public class ProductoMySQL implements ProductoDAO{
+    private Connection con;
+    private ResultSet rs;
+    private CallableStatement cs;
     
+    @Override
+    public ArrayList<Producto> listarTodas() {
+        ArrayList<Producto> productos = new ArrayList<>();
+        try{
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("");
+            rs = cs.executeQuery();
+            while(rs.next()){
+                Producto producto = new Producto();
+
+                productos.add(producto);
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return productos;               
+    }
+
+    @Override
+    public int insertar(Producto producto) {
+        int resultado = 0;
+        try{
+            con =DBManager.getInstance().getConnection();
+            cs = con.prepareCall("");
+            cs.registerOutParameter("", java.sql.Types.INTEGER);
+
+            cs.executeUpdate();
+            producto.setIdProducto(cs.getInt(""));                
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;           
+    }
+
+    @Override
+    public int modificar(Producto producto) {
+        int resultado = 0;
+        try{
+            con =DBManager.getInstance().getConnection();
+            cs = con.prepareCall("");
+
+            cs.executeUpdate();      
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;            
+    }
+
+    @Override
+    public int eliminar(int idproducto) {
+        int resultado = 0;
+        try{
+            con =DBManager.getInstance().getConnection();
+            cs = con.prepareCall("");
+            cs.setInt("",idproducto);
+            cs.executeUpdate();
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;          
+    }
+
 }
