ad471364c01c329263886e969898da62e730fcff
MauArenales <arenales.mauricio24@gmail.com>
2023-06-17 00:30:01 -0500

    ningun cambio

--- ARSACSoftOrdenesController/src/pe/edu/pucp/arcacsoft/ordenes/dao/OrdenesDAO.java
+++ ARSACSoftOrdenesController/src/pe/edu/pucp/arcacsoft/ordenes/dao/OrdenesDAO.java
@@ -1,14 +1,5 @@
 package pe.edu.pucp.arcacsoft.ordenes.dao;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author El~Nicolays
- */
 public class OrdenesDAO {
     
 }
