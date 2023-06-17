741c7c6c9e1dc90a3d4426969b03b6d54c2b0312
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-17 00:03:10 -0500

    .
    .

---
+++ ARSACSoftOrdenesController/src/pe/edu/pucp/arsacsoft/proveedores/mysql/prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.arsacsoft.proveedores.mysql;
+
+import prueba.*;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class prueba {
+    
+}
