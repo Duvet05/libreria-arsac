460877fa30db7c45a1b40eb4828c56ec04d86f1c
NicoLay303 <p.david@pucp.edu.pe>
2023-06-17 00:04:41 -0500

    Dao package

--- ARSACSoftOrdenesController/src/pe/edu/pucp/arcacsoft/ordenes/dao/OrdenesDAO.java
+++ ARSACSoftOrdenesController/src/pe/edu/pucp/arcacsoft/ordenes/dao/OrdenesDAO.java
@@ -1,15 +1,14 @@
+package pe.edu.pucp.arcacsoft.ordenes.dao;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.pucp.arsacsoft.proveedores.mysql;
-
-import prueba.*;
 
 /**
  *
- * @author Gonzalo
+ * @author El~Nicolays
  */
-public class prueba {
+public class OrdenesDAO {
     
 }
