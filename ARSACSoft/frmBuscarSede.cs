4c18de971dbe156dc6f01cd70340d98115537a9a
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-18 15:13:40 -0500

    verificaciones de sede

--- ARSACSoft/frmBuscarSede.cs
+++ ARSACSoft/frmBuscarSede.cs
@@ -15,35 +15,45 @@ namespace ARSACSoft
             daoSede = new SedesWSClient();
 
             dgvSedes.AutoGenerateColumns = false;
-            dgvSedes.DataSource = daoSede.listarSedes();
-
+            var sedes = daoSede.listarSedes();
+            if (sedes != null)
+            {
+                dgvSedes.DataSource = sedes;
+            }
+            else
+            {
+                // Maneja la situación en la que 'listarSedes()' devuelve null
+            }
         }
 
         public sede SedeSeleccionada { get => sedeSeleccionada; set => sedeSeleccionada = value; }
 
         private void btnSeleccionar_Click(object sender, EventArgs e)
         {
-
-            if (dgvSedes.CurrentRow != null)
+            if (dgvSedes.CurrentRow != null && dgvSedes.CurrentRow.DataBoundItem is sede)
             {
                 sedeSeleccionada = (sede)dgvSedes.CurrentRow.DataBoundItem;
                 this.DialogResult = DialogResult.OK;
             }
-
+            else
+            {
+                // Maneja la situación en la que no hay fila actual seleccionada o el elemento enlazado no es una 'sede'
+            }
         }
 
         private void dgvSedes_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
-            sede sede = (sede)dgvSedes.Rows[e.RowIndex].DataBoundItem;
-
-            dgvSedes.Rows[e.RowIndex].Cells[0].Value =
-                sede.idSede.ToString();
-            dgvSedes.Rows[e.RowIndex].Cells[1].Value =
-                sede.direccion;
-            dgvSedes.Rows[e.RowIndex].Cells[2].Value =
-                sede.esPrincipal ? "SI" : "NO";
-            dgvSedes.Rows[e.RowIndex].Cells[3].Value =
-                sede.correo;
+            if (dgvSedes.Rows[e.RowIndex].DataBoundItem is sede sede)
+            {
+                dgvSedes.Rows[e.RowIndex].Cells[0].Value = sede.idSede.ToString();
+                dgvSedes.Rows[e.RowIndex].Cells[1].Value = sede.direccion;
+                dgvSedes.Rows[e.RowIndex].Cells[2].Value = sede.esPrincipal ? "SI" : "NO";
+                dgvSedes.Rows[e.RowIndex].Cells[3].Value = sede.correo;
+            }
+            else
+            {
+                // Maneja la situación en la que el elemento enlazado no es una 'sede'
+            }
         }
     }
 }
