bc1f9e806acf7f5582daff72fc6696ff57bc92eb
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-18 23:43:28 -0500

    .

---
+++ ARSACSoftWebServices/src/java/pe/edu/pucp/arsacsoft/services/AlmacenWS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package pe.edu.pucp.arsacsoft.services;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Gonzalo
+ */
+@WebService(serviceName = "AlmacenWS")
+public class AlmacenWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
