0f5c0c89d731aba51b1057ca9e6b016cd960e980
NicoLay303 <81640124+NicoLay303@users.noreply.github.com>
2023-06-20 20:09:32 -0500

    sp_proveedor actualizao

--- Scripts/sp_proveedor.sql
+++ Scripts/sp_proveedor.sql
@@ -1,7 +1,8 @@
--- PROVEEDOR
+-- #######################################################################
+-- PROVEEDOR - INSERTAR
+-- #######################################################################
 DROP PROCEDURE IF EXISTS INSERTAR_PROVEEDOR;
-DROP PROCEDURE IF EXISTS LISTAR_PROVEEDORES_POR_NOMBRE_RUC;
-
+DROP PROCEDURE IF EXISTS LISTAR_PROVEEDORES_POR_NOMBRE;
 DROP PROCEDURE IF EXISTS ACTUALIZAR_PROVEEDOR;
 DROP PROCEDURE IF EXISTS ELIMINAR_PROVEEDOR;
 
@@ -16,19 +17,37 @@ CREATE PROCEDURE INSERTAR_PROVEEDOR(
 BEGIN
 	INSERT INTO proveedor(nombre, direccion, telefono, RUC, activo)
     VALUES(_nombre, _direccion, _telefono, _RUC, true);
-	SET _id_proveedor = @@last_insert_id;
-END $
+	SET _id_proveedor = last_insert_id();
+END; $
 
-delimiter $
-CREATE PROCEDURE LISTAR_PROVEEDORES_POR_NOMBRE_RUC(
-	IN _nombre VARCHAR(1000)
+-- #######################################################################
+-- PROVEEDOR - LISTAR POR NOMBRE
+-- #######################################################################
+DELIMITER $
+CREATE PROCEDURE ORDENAR_PROVEEDOR_ALFABETICAMENTE()
+BEGIN
+    SELECT * FROM proveedor
+    ORDER BY nombre;
+END;$
+
+-- #######################################################################
+-- PROVEEDOR - LISTAR POR categoria
+-- #######################################################################
+DELIMITER $
+CREATE PROCEDURE ORDENAR_PROVEEDOR_CATEGORIA_PRODUCTO(
+IN _id_categoria INT
 )
 BEGIN
-	SELECT id_proveedor, nombre, direccion, RUC, telefono
-    from proveedor
-    where (nombre LIKE CONCAT('%',_nombre,'%') or RUC LIKE CONCAT('%',_nombre,'%'))and activo = 1;
-END $
-
+    SELECT DISTINCT proveedor.*
+    FROM proveedor
+    JOIN productoXproveedor ON proveedor.id_proveedor = productoXproveedor.fid_proveedor
+    JOIN producto ON productoXproveedor.fid_producto = producto.id_producto
+    WHERE producto.fid_categoria = _id_categoria;
+END;$
+-- #######################################################################
+-- PROVEEDOR - ACTUALIZAR/MODIFICAR
+-- #######################################################################
+DELIMITER $
 CREATE PROCEDURE ACTUALIZAR_PROVEEDOR(
 	IN _id_proveedor INT,
 	IN _nombre VARCHAR(100),
@@ -43,8 +62,11 @@ BEGIN
     telefono = _telefono,
     RUC = _RUC
     where id_proveedor = _id_proveedor;
-END $
-
+END; $
+-- #######################################################################
+-- PROVEEDOR -ELIMINAR
+-- #######################################################################
+DELIMITER $
 CREATE PROCEDURE ELIMINAR_PROVEEDOR(
 	IN _id_proveedor INT
 )
@@ -52,6 +74,18 @@ BEGIN
 	UPDATE proveedor set
 	activo = false
     where id_proveedor = _id_proveedor;
-END $
+END; $
 
--- PRODUCTO X PROVEEDOR
+-- #######################################################################
+-- INSERTAR PRODUCTO X PROVEEDOR
+-- #######################################################################
+DELIMITER $
+CREATE PROCEDURE INSERTAR_PRODUCTO_DE_PROVEEDOR(
+    IN _fid_producto INT,
+    IN _fid_proveedor INT,
+    IN _costo DECIMAL(10, 2)
+)
+BEGIN
+    INSERT INTO productoXproveedor (fid_producto, fid_proveedor, costo)
+    VALUES (_fid_producto, _fid_proveedor, _costo);
+END;$
