3c70f50d2178975dd5d732d197f61080b3dfd8be
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-06-27 05:36:19 -0500

    ..

--- ARSACSoft/frmPrincipal.Designer.cs
+++ ARSACSoft/frmPrincipal.Designer.cs
@@ -490,6 +490,7 @@
             this.Controls.Add(this.panelBarraVentana);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmPrincipal";
             this.Text = "frmPrincipal";
